3d79847a77d816ea868a7bc747cd4d3b2f1f134c
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2021-05-08 18:11:44 -0500

    [add] Componente HelloWorld de Vue

--- app/App.vue
+++ app/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="ui container">
         <div class="ui secondary menu">
-            <a class="item"> Home </a>
-            <a class="active item">project</a>
+            <a class="active item"> Home </a>
+            <a class="item">project</a>
             <a class="item"> profile </a>
             <div class="right menu">
                 <div class="item">
@@ -11,16 +11,21 @@
                         <i class="search link icon"></i>
                     </div>
                 </div>
+                <a class="item"> login </a>
             </div>
         </div>
         <h1> {{ message }} </h1>
-
+        <HelloWorld msg="asdasd" />
     </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue';
 export default {
     name: 'App',
+    components:{
+        HelloWorld
+    },
     data() {
         return{
             message: 'App Sistema Biblioteca'
---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{msg}}
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed quas placeat dolorum voluptatem maiores recusandae eum consectetur iusto excepturi, necessitatibus, numquam omnis dolores, debitis consequatur laborum suscipit architecto tempore facilis.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props:{
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
