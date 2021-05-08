298748f079a313e07e6144fc5b0928eff975522e
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2021-05-07 19:30:48 -0500

    [refactor] Reordenando codigo
    Eliminando configuracion de motor de plantilla con pugjs

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send('respond with a resource home');
 });
 
-router.get('/login', (req, res,next)=>{
-  res.render('login', {
+router.get('/login', (req, res, next)=>{
+  /**res.render('login', {
     title: 'Signup',
     login: 'login acces'
-  });
+  });**/
+  res.send('dsfsdf');
 });
 
 module.exports = router;
